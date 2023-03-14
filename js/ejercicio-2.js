1af48f448f50adc6c307419f09bd4a2255d5772b
juanmllanos <juan_m@hotmail.com.ar>
2023-03-14 20:06:31 -0300

    ejercicio 2 terminado

---
+++ js/ejercicio-2.js
@@ -0,0 +1,30 @@
+const ciudades = [];
+
+do {
+	ciudades.push(prompt('Ingrese el nombre de una ciudad:'));
+} while (confirm());
+document.write(`<ul>`);
+for (let i = 0; i < ciudades.length; i++) {
+	document.write(`<li>${ciudades[i]}</li>`);
+};
+
+document.write(`</ul>`);
+
+document.write(`Longitud del arreglo: ${ciudades.length}`);
+console.log(ciudades[0], ciudades[2], ciudades[ciudades.length - 1]);
+ciudades.push('Paris');
+
+document.write(`<ul>`);
+for (let i = 0; i < ciudades.length; i++) {
+	document.write(`<li>${ciudades[i]}</li>`);
+};
+document.write(`</ul>`);
+
+document.write(`La segunda ciudad en el arreglo es: ${ciudades[1]}`);
+ciudades[1] = 'Barcelona';
+
+document.write(`<ul>`);
+for (let i = 0; i < ciudades.length; i++) {
+	document.write(`<li>${ciudades[i]}</li>`);
+};
+document.write(`</ul>`);
