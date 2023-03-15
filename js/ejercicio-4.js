09212b63d10b057db7e3da64b5a1a900a6ebf564
juanmllanos <juan_m@hotmail.com.ar>
2023-03-14 22:10:29 -0300

    ejercicio 5 terminado

---
+++ js/ejercicio-4.js
@@ -0,0 +1,9 @@
+function numeroACadena(numero) {
+	if (numero % 2 === 0) {
+		return document.write(`El numero ${numero} es par.`)
+	}
+	if (numero % 2 === 1) {
+		return document.write(`El numero ${numero} es impar.`)
+	};
+};
+numeroACadena(25);
